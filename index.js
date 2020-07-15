da92069fd719bc92919ef90554570315d7e2bf61
jlorenc1 <joshua.lorenc@icloud.com>
2020-07-15 09:57:07 -0700

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,19 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(string) {
+  let time = parseInt(string.split(":")[0])
+
+  if (time < 12) {
+    return "Good Morning"
+  } else if (time < 17) {
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+}
+
+function displayMessage(string) {
+  document.getElementById("greeting").innerText = string;
+}
